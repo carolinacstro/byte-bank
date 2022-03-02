55bcad02856c30aca99c86603e8f19d57e17d9c4
Carolina Castro <carolinacoliveira@outlook.com>
2022-03-02 14:24:52 -0300

    add package.json e classes

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-class Cliente {
-    nome;
-    cpf;
-    agencia;
-    saldo;
-    rg;
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-class ContaCorrente {
-    agencia;
-    saldo;
-
-    sacar(valor) {
-        if (this.saldo >= valor) {
-                this.saldo -= valor;
-        }
-    }
-}
-
+// clientes
 const cliente1 = new Cliente();
 cliente1.nome = "Isadora";
 cliente1.cpf = 11122233309;
@@ -25,18 +10,19 @@ const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
+
+// cliente 1
 const contaCorrenteIsadora = new ContaCorrente();
-contaCorrenteIsadora.saldo = 0;
 contaCorrenteIsadora.agencia = 1001;
 
-// movimentações conta corrente
-console.log(contaCorrenteIsadora.saldo);
-console.log(contaCorrenteIsadora.saldo = 100);
-contaCorrenteIsadora.sacar(200);
-console.log(contaCorrenteIsadora.saldo);
+// movimentações conta corrente - cliente 1
+contaCorrenteIsadora.depositar(100);
+contaCorrenteIsadora.depositar(100);
+contaCorrenteIsadora.depositar(100);
 
+const valorSacado = contaCorrenteIsadora.sacar(50);
+console.log(valorSacado);
 
-// console.log(cliente1);
-// console.log(cliente2);
+console.log(contaCorrenteIsadora);
 
 
